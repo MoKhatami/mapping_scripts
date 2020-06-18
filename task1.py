af94d4f957e00534c1e59ffbe5eeaecfb5beb141
Arjun Rao <arjun.rao@duke.edu>
2020-06-18 14:43:38 -0400

    first error

--- task1.py
+++ task1.py
@@ -1,4 +1,16 @@
-from skimage import draw, io
-import numpy as np
+#from skimage import draw, io
 import json
 import os
+from cv2 import cv2
+import numpy as np
+from PIL import Image
+
+
+img = Image.open("firstImage.png").save("sample1.bmp")
+
+image = cv2.imread("sample1.bmp")
+template = image[30:40,30:40,:]
+
+result = cv2.matchTemplate(image,template,cv2.TM_CCOEFF_NORMED)
+
+print(np.unravel_index(result.argmax(),result.shape))
