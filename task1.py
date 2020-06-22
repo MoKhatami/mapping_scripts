539c52d83f72c2e96ed225cba8589f9bf78565a4
Mo Khatami <mokhatami113@gmail.com>
2020-06-22 12:02:52 -0400

    edited task

--- task1.py
+++ task1.py
@@ -1,16 +1,29 @@
-#from skimage import draw, io
-import json
-import os
-from cv2 import cv2
-import numpy as np
-from PIL import Image
+import cv2
 
+method = cv2.TM_SQDIFF_NORMED
 
-img = Image.open("firstImage.png").save("sample1.bmp")
+# Read the images from the file
+small_image = cv2.imread('firstImage.png')
+large_image = cv2.imread('secondImage.png')
 
-image = cv2.imread("sample1.bmp")
-template = image[30:40,30:40,:]
+result = cv2.matchTemplate(firstImage, secondImage, method)
 
-result = cv2.matchTemplate(image,template,cv2.TM_CCOEFF_NORMED)
+# We want the minimum squared difference
+mn, _, mnLoc, _ = cv2.minMaxLoc(result)
 
-print(np.unravel_index(result.argmax(),result.shape))
+# Draw the rectangle:
+# Extract the coordinates of our best match
+MPx, MPy = mnLoc
+
+# Step 2: Get the size of the template. This is the same size as the match.
+trows, tcols = secondImage.shape[:2]
+
+# Step 3: Draw the rectangle on large_image
+cv2.rectangle(large_image, (MPx, MPy),
+              (MPx + tcols, MPy + trows), (0, 0, 255), 2)
+
+# Display the original image with the rectangle around the match.
+cv2.imshow('output', large_image)
+
+# The image is only displayed if we call this
+cv2.waitKey(0)
