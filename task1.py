5a91598d4cdf42624d2521e2be30f61e41e6b8ed
Mo Khatami <mokhatami113@gmail.com>
2020-06-23 10:59:08 -0400

    "tested using PIL and Image module"

--- task1.py
+++ task1.py
@@ -1,29 +1,32 @@
-import cv2
+import numpy as np
+from PIL import Image
 
-method = cv2.TM_SQDIFF_NORMED
+im_big = Image.open(r"/Users/mokha/Sites/mapping_scripts/first1.png")
+im_small = Image.open(r"/Users/mokha/Sites/mapping_scripts/first2.png")
 
-# Read the images from the file
-small_image = cv2.imread('firstImage.png')
-large_image = cv2.imread('secondImage.png')
 
-result = cv2.matchTemplate(firstImage, secondImage, method)
+def find_matches(big, small):
+    arr_h = np.asarray(big)
+    arr_n = np.asarray(small)
 
-# We want the minimum squared difference
-mn, _, mnLoc, _ = cv2.minMaxLoc(result)
+    y_h, x_h = arr_h.shape[:2]
+    y_n, x_n = arr_n.shape[:2]
 
-# Draw the rectangle:
-# Extract the coordinates of our best match
-MPx, MPy = mnLoc
+    xstop = x_h - x_n + 1
+    ystop = y_h - y_n + 1
 
-# Step 2: Get the size of the template. This is the same size as the match.
-trows, tcols = secondImage.shape[:2]
+    matches = []
+    for xmin in range(0, xstop):
+        for ymin in range(0, ystop):
+            xmax = xmin + x_n
+            ymax = ymin + y_n
 
-# Step 3: Draw the rectangle on large_image
-cv2.rectangle(large_image, (MPx, MPy),
-              (MPx + tcols, MPy + trows), (0, 0, 255), 2)
+            arr_s = arr_h[ymin:ymax, xmin:xmax]     # Extract subimage
+            arr_t = (arr_s == arr_n)                # Create test matrix
+            if arr_t.all():                         # Only consider exact matches
+                matches.append((xmin, ymin))
 
-# Display the original image with the rectangle around the match.
-cv2.imshow('output', large_image)
+    return matches
 
-# The image is only displayed if we call this
-cv2.waitKey(0)
+
+print(find_matches(im_big, im_small))
