0d281d9e34cceaef6ff2a8378041b671832b3d39
Mo Khatami <mokhatami113@gmail.com>
2020-06-26 15:03:04 -0400

    updated task1

--- first-task/task1.py
+++ first-task/task1.py
@@ -10,49 +10,40 @@ import zlib
 import io
 import base64
 
+img_dir = []
+for filename in glob.glob('../../piconha2_jpg'):
+    im=Image.open(filename)
+    image_dir.append(im)
 
 data_dir = os.path.join('.','Book of Fortresses','Book of Fortresses')
 ann_dir = os.path.join(data_dir,'ann')
-img_dir = os.path.join(data_dir,'img')
-mask_dir = os.path.join(data_dir,'masked_features')
-
-# Grabbed poly2mask function from:
-# https://github.com/scikit-image/scikit-image/issues/1103#issuecomment-52378754
-
-def poly2mask(vertex_row_coords, vertex_col_coords, shape):
-    fill_row_coords, fill_col_coords = draw.polygon(vertex_row_coords, vertex_col_coords, shape)
-    mask = np.zeros(shape, dtype=np.uint8)
-    mask[fill_row_coords, fill_col_coords] = 255
-    return mask
-
-# https://docs.supervise.ly/data-organization/import-export/supervisely-format#bitmap
-# Heeded a warning and changed `np.fromstring()` to `np.frombuffer()` and seems to work fine
-
-def base64_2_mask(s):
-    z = zlib.decompress(base64.b64decode(s))
-    n = np.frombuffer(z, np.uint8)
-    mask = cv2.imdecode(n, cv2.IMREAD_UNCHANGED)[:, :, 3]
-    return mask
-
-# Not using this one here, but including it for reference
-def mask_2_base64(mask):
-    img_pil = Image.fromarray(np.array(mask, dtype=np.uint8))
-    img_pil.putpalette([0,0,0,255,255,255])
-    bytes_io = io.BytesIO()
-    img_pil.save(bytes_io, format='PNG', transparency=0, optimize=0)
-    bytes = bytes_io.getvalue()
-    return base64.b64encode(zlib.compress(bytes)).decode('utf-8')
-
-try:
-    os.mkdir(mask_dir)
-except OSError:
-    if not os.path.exists(mask_dir):
-        sys.exit("Error creating: " + mask_dir)
 
 ann_list = glob.glob(os.path.join(ann_dir,'*.json'))
 
-im_big = Image.open(r"/Users/mokha/Sites/mapping_scripts/first-task/first1.png")
-im_small = Image.open(r"/Users/mokha/Sites/mapping_scripts/first-task/first2.png")
+for ann_path in ann_list:
+
+    print(ann_path)
+
+    # remove .json extension – should maybe use pathlib instead...
+    img_filename = os.path.splitext(os.path.basename(ann_path))[0]
+    # replace . with _ for mask subdirectory name
+    mask_subdir = img_filename.replace('.','_')
+    mask_subdir_path = os.path.join(mask_dir, mask_subdir)
+    # remove .jpg extension
+    img_name = os.path.splitext(img_filename)[0]
+    img_path = os.path.join(img_dir, img_filename)
+
+    img = skio.imread(img_path)
+
+    try:
+        os.mkdir(mask_subdir_path)
+    except OSError:
+        if not os.path.exists(mask_subdir_path):
+            sys.exit("Error creating: " + mask_subdir_path)
+
+    with open(ann_path,'r') as f:
+        annotations = json.load(f)
+
 
 
 def find_matches(big, small):
