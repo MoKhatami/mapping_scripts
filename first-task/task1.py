90b664b9d553bce59cbd1788b2d635676880e4fb
Mo Khatami <mokhatami113@gmail.com>
2020-06-29 12:30:35 -0400

    organized

--- first-task/task1.py
+++ first-task/task1.py
@@ -10,29 +10,20 @@ import zlib
 import io
 import base64
 
+
+#create image directory
 directories = "./piconha2_jpg"
-for root, dirs, files in os.walk(directories):
-    print(files)
-    img_dir.append(files)
-    print(len(img_dir))
 
 #create directory for JSON files from supervisely
 data_dir = os.path.join('.','Book of Fortresses','Book of Fortresses')
 ann_dir = os.path.join(data_dir,'ann')
 ann_list = glob.glob(os.path.join(ann_dir,'*.json'))
 
-#iterate over JSON files
-for ann_path in ann_list:
-    print(ann_path)
-    with open(ann_path,'r') as f:
-        annotations = json.load(f)
-
-
 #method to check for image overlap
-def find_matches(big, small):
+def find_matches(container, object):
     #convert images to arrays
-    arr_h = np.asarray(big)
-    arr_n = np.asarray(small)
+    arr_h = np.asarray(container)
+    arr_n = np.asarray(object)
     print(arr_h)
 
     y_h, x_h = arr_h.shape[:2]
@@ -56,15 +47,24 @@ def find_matches(big, small):
 
 
 def main():
+
+    #iterate over image directory
     img_dir = []
     for root, dirs, files in os.walk(directories):
         #print out files
         for file in files:
-            img_dir.append(files)
-            print(files)
+            img_dir.append(file)
+            print(file)
         print(len(img_dir))
 
     for i in range(len(img_dir) - 1):
         print(find_matches(img_dir[i], img_dir[i+1]))
+
+
+    #iterate over JSON directory
+    for ann_path in ann_list:
+        print(ann_path)
+        with open(ann_path,'r') as f:
+            annotations = json.load(f)
 
 main()
