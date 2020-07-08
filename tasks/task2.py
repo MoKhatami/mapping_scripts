7c37ea42310bb230c732a60ce47fa5aa810bb8a4
Mo Khatami <mokhatami113@gmail.com>
2020-07-08 11:16:18 -0400

    "task2.py can now read in JSON files and add files to an iterable list"

---
+++ tasks/task2.py
@@ -0,0 +1,34 @@
+from skimage import draw
+from skimage import io as skio
+import numpy as np
+import json
+import os
+import glob
+from os import path
+from PIL import Image
+import zlib
+import io
+import base64
+import numpy.ma as ma
+import cv2
+
+
+class Supervisely_polygon:
+    def _init_(variables):
+
+#create directory
+files = []
+path = 'ann'
+
+#iterate over JSONS and print filenames
+for filename in glob.glob(os.path.join(path, '*.json')): #only process .JSON files in folder.]\
+    print(filename)
+
+    #open each JSON file
+    with open(filename, encoding='utf-8', mode='r') as currentFile:
+        data = currentFile.read()
+        keyword = json.loads(data)
+        files.append(keyword)
+
+for i in files:
+    print(i)
