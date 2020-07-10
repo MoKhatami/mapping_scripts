639405d88e4b9b00954a3755a93f7c3f18418e22
Mo Khatami <mokhatami113@gmail.com>
2020-07-10 13:55:23 -0400

    "task2.py can now read in an instanceURI from Labelbox and return b64 and origin"

--- tasks/task2.py
+++ tasks/task2.py
@@ -1,6 +1,8 @@
 from skimage import draw
 from skimage import io as skio
+import skimage
 import numpy as np
+from matplotlib.pyplot import imread
 import json
 import os
 import glob
@@ -8,21 +10,41 @@ from os import path
 from PIL import Image
 import zlib
 import io
-import base64
 import numpy.ma as ma
 import cv2
+import base64
+import requests
 
 
-class Supervisely_polygon:
-    def _init_(variables):
+def bbox2(img):
+    rows = np.any(img, axis=1)
+    cols = np.any(img, axis=0)
+    try:
+        rmin, rmax = np.where(rows)[0][[0, -1]]
+        cmin, cmax = np.where(cols)[0][[0, -1]]
+    except IndexError:
+        # NOTE: Not sure this is the right decision on what to return for blank mask
+        rmin, rmax = (0,0)
+        cmin, cmax = (0,0)
+    return (rmin,rmax+1), (cmin,cmax+1)
 
+def mask_2_base64(mask):
+    img_pil = Image.fromarray(np.array(mask, dtype=np.uint8))
+    img_pil.putpalette([0,0,0,255,255,255])
+    bytes_io = io.BytesIO()
+    img_pil.save(bytes_io, format='PNG', transparency=0, optimize=0)
+    bytes = bytes_io.getvalue()
+    return base64.b64encode(zlib.compress(bytes)).decode('utf-8')
+
+#Label, objects, instanceURI
 #create directory
 files = []
-path = 'ann'
+filenames = []
+path = 'labelbox'
+map_dicts = []
 
 #iterate over JSONS and print filenames
 for filename in glob.glob(os.path.join(path, '*.json')): #only process .JSON files in folder.]\
-    print(filename)
 
     #open each JSON file
     with open(filename, encoding='utf-8', mode='r') as currentFile:
@@ -30,5 +52,24 @@ for filename in glob.glob(os.path.join(path, '*.json')): #only process .JSON fil
         keyword = json.loads(data)
         files.append(keyword)
 
-for i in files:
-    print(i)
+    filenames.append(filename)
+
+
+#for i in files:
+    #print(i['size']['height'])
+
+label_box = files[0]
+uri = label_box[1]['Label']['objects'][0]['instanceURI']
+id = label_box[1]['Label']['objects'][0]['featureId']
+print(uri)
+
+print('\t * Downloading mask image from server')
+# Delay just so we don't get kicked off of the server...
+mask_img = imread(uri)
+
+origin = bbox2(mask_img)
+
+data = base64.b64encode(requests.get(uri).content)
+
+print(origin)
+print(data)
